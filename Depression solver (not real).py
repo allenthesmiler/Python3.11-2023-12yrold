d873ac4c6c3517651e9a4925e0cb3e786788c121
allenthesmiler <130497856+allenthesmiler@users.noreply.github.com>
2023-05-07 15:32:45 +0400

    Depressionsolver.py

---
+++ Depression solver (not real).py
@@ -0,0 +1,19 @@
+#python 3.11
+# Allen's Depresstion helper (this does not help with depresstion)
+ok = input("Are you ok? ")
+# male or female
+morf = input("are you a male or female? ")
+if ok == "no" and morf == "male":
+    print("Damn bro, well it isss what it isss...")
+if ok == "yes" and morf == "male":
+    print("AYY LETS GO No More DEPRESSION!")
+if ok == "no" and morf == "female":
+    print("ay, your gonna be ok, im here")
+if ok == "yes" and morf == "female":
+    print("are you sure")
+    #I know women sometimes lie
+    okhm = input("are you really ok? ")
+    if okhm == "no":
+        print("ay, your gonna be ok, im here")
+    if okhm == "yes":
+        print("OK CHILL!")
